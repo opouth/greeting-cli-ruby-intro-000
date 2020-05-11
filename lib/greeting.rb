70990a3f9c94adb1e905f55dd0264285100ed737
Ermite Rikudo <opouth@hotmail.fr>
2020-05-11 20:37:23 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,7 +1,7 @@
 # code the #greeting method here!
 #gets.strip
 "Hi! I'm HAL, what's your name?"
-name = STDIN.gets.chomp
+$name = STDIN.gets.chomp
 def greeting(name)
   puts "Hello #{name}. It's nice to meet you."
 end
